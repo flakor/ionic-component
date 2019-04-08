7b3b66957b5772a88e6fa62db951ed01ba9ad1d6
Pablo Ortiz Barra <portiz@dgtm.cl>
2019-04-08 15:46:55 -0400

    componentes lista, action-sheet, y alert

---
+++ src/app/pages/action-sheet/action-sheet.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from 'src/app/components/header/header.component';
+
+@Component({
+  selector: 'app-action-sheet',
+  templateUrl: './action-sheet.page.html',
+  styleUrls: ['./action-sheet.page.scss'],
+})
+export class ActionSheetPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
